f246da40149666575fd6d9b7785a122032d99bca
weiyg <weiyg@asiainfo.com>
2020-12-17 16:09:35 +0800

    multi province remove useless package

--- multi_province/src/main/java/com/wygdove/multiprovince/dao/mapper/beijing/GsopSysParamSMapper.java
+++ multi_province/src/main/java/com/wygdove/multiprovince/dao/mapper/beijing/GsopSysParamSMapper.java
@@ -3,7 +3,6 @@ package com.wygdove.multiprovince.dao.mapper.beijing;
 import com.wygdove.multiprovince.model.gsopsysparam.GsopSysParamQueryRequest;
 import com.wygdove.multiprovince.model.gsopsysparam.GsopSysParamVO;
 import java.util.List;
-import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @Title: GsopSysParamSMapper
--- multi_province/src/main/java/com/wygdove/multiprovince/dao/mapper/common/GsopWorkerOrderSMapper.java
+++ multi_province/src/main/java/com/wygdove/multiprovince/dao/mapper/common/GsopWorkerOrderSMapper.java
@@ -4,7 +4,6 @@ package com.wygdove.multiprovince.dao.mapper.common;
 import com.wygdove.multiprovince.model.gsopwoordinfo.GsopWoordInfoQueryRequest;
 import com.wygdove.multiprovince.model.gsopwoordinfo.GsopWoordInfoVO;
 import java.util.List;
-import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @Title: GsopWorkerOrderSMapper
